fe4772b1132d5478b3b0e1de91d374f5b7a79da2
Jan Dvorak <storytellercz@proton.me>
2024-02-05 17:03:36 +0100

    Meteor 3 compat

--- server/apollo.js
+++ server/apollo.js
@@ -1,11 +1,11 @@
 import { ApolloServer } from '@apollo/server';
-import { WebApp } from 'meteor/webapp';
+import { WebApp, WebAppInternals } from 'meteor/webapp';
 import { getUser } from 'meteor/apollo';
 import { LinksCollection } from '/imports/api/links';
 import typeDefs from '/imports/apollo/schema.graphql';
-import express from 'express';
 import { expressMiddleware } from '@apollo/server/express4';
-import { json } from 'body-parser';
+
+const express = WebAppInternals.NpmModules.express.module;
 
 const resolvers = {
   Query: {
@@ -27,12 +27,9 @@ const server = new ApolloServer({
 export async function startApolloServer() {
   await server.start();
 
-  WebApp.expressHandlers.use(
-    '/graphql',                                     // Configure the path as you want.
-    express()                                       // Create new Express router.
-      .disable('etag')                              // We don't server GET requests, so there's no need for that.
-      .disable('x-powered-by')                      // A small safety measure.
-      .use(json())                                  // From `body-parser`.
-      .use(expressMiddleware(server, { context }))  // From `@apollo/server/express4`.
+  WebApp.handlers.use(
+    '/graphql',                            // Configure the path as you want.
+    express.json(),
+    expressMiddleware(server, { context }) // From `@apollo/server/express4`
   );
 }
